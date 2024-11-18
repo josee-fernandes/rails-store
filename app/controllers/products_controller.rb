fbf5edb067b017da0cff7891c054af425fe4af6d
José Vitor <jose-vitor-7@hotmail.com>
2024-11-18 00:43:21 -0300

    feat: creating search field

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,20 +1,20 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.order(name: :desc).limit(5)
+    @products = Product.order(name: :asc).limit(6)
     @products_with_discount = Product.order(:price).limit(1)
   end
 
   def create
     product = params.require(:product).permit(:name, :description, :price, :amount)
     Product.create(product)
-    
+
     redirect_to root_url
   end
 
   def destroy
     id = params[:id]
 
-    product = Product.destroy(id)
+    Product.destroy(id)
 
     redirect_to root_url
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   resources :products, only: [:new, :create, :destroy]
+
+  get 'products/search', to: 'products#search', as: :search_product
 end
