1c4818f4823893ca991732de4f2cf1432cc92efc
José Vitor <jose-vitor-7@hotmail.com>
2024-11-17 22:54:42 -0300

    feat: products filters + change root

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def index
+    @products = Product.order(name: :desc).limit(2)
+    @products_with_discount = Product.order(:price).limit(1)
+  end
 end
