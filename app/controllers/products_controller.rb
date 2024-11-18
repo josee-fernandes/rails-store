511a6bed6b133d9ddb40f2c72248b0fd31ded76b
José Vitor <jose-vitor-7@hotmail.com>
2024-11-17 23:39:04 -0300

    feat: creating new products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,12 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.order(name: :desc).limit(2)
+    @products = Product.order(name: :desc).limit(5)
     @products_with_discount = Product.order(:price).limit(1)
+  end
+
+  def create
+    product = params.require(:product).permit(:name, :description, :price, :amount)
+    Product.create(product)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'products#index'
 
   get 'products/new', to: 'products#new'
+  post 'products', to: 'products#create'
 end
