eb37b655f7e2f7db943f9bbb1a4d50223295069b
José Vitor <jose-vitor-7@hotmail.com>
2024-11-25 00:16:50 -0300

    feat: add department relationship + render on product creation page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  belongs_to :department
+
   validates :price, presence: true
   validates :name, length: { minimum: 4 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_25_025345) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_25_030739) do
   create_table "departments", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -24,6 +24,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_25_025345) do
     t.integer "amount"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "department_id"
   end
 
 end
